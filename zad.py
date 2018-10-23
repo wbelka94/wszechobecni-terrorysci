ee7f50604ff41c51a06b0b223d0e2d4e2ab65e3f
wbelka94 <wbelka94@gmail.com>
2018-10-23 22:25:03 +0200

    avg results

--- zad.py
+++ zad.py
@@ -1,35 +1,56 @@
 import itertools
 import random
 
-n = 10000  # liczba osob
-p = 0.1  # prawdopodobienstwo nocowania w hotelu
-h = 100  # liczba hoteli
-d = 100  # liczba dni
 
-m = {} #slownik par ?
-histogram = {}
-suspectsPersons = set()
-suspectsPairs = set()
-suspectsPersonsAndDays = 0
-for di in range(d):                                         # iterujemy po dniach
-    hs = {}  # hotel state                                    lista osob w hotelu
-    for ni in range(n):                                     # iterujemy po osobach
-        if random.random() < p:                             # sprawdzamy czy di dnia ni osoba poszla do hotelu
-            nh = random.randint(0, h)                       # losujemy numerek hotelu do którego idzie
-            hs[nh] = hs.get(nh, []) + [ni]                  # dopisujemy ta osobe do listy osob w hotelu
-    for v in hs.values():                                   # iterujemy po hotelach
-        for pair in list(itertools.combinations(v, 2)):     # iterujemy po wszystkich parach w v hotelu
-            m[pair] = m.get(pair, 0) + 1                    # zwiekszamy licznik spotkania sie konkretnej pary
+def simulate():
+    m = {}
+    histogram = {}
+    suspects_persons = set()
+    suspects_pairs = set()
+    suspects_persons_and_days = 0
+    for di in range(d):
+        hs = {}
+        for ni in range(n):
+            if random.random() < p:
+                nh = random.randint(0, h)
+                hs[nh] = hs.get(nh, []) + [ni]
+        for v in hs.values():
+            for pair in list(itertools.combinations(v, 2)):
+                m[pair] = m.get(pair, 0) + 1
 
-for k,v in m.items():
-    histogram[v] = histogram.get(v, 0) + 1
-    if v >= 2:
-        suspectsPersonsAndDays += v*(v-1)/2
-        suspectsPersons.add(k[0])
-        suspectsPersons.add(k[1])
-        suspectsPairs.add(k)
+    for k, v in m.items():
+        histogram[v] = histogram.get(v, 0) + 1
+        if v >= 2:
+            suspects_persons_and_days += v * (v - 1) / 2
+            suspects_persons.add(k[0])
+            suspects_persons.add(k[1])
+            suspects_pairs.add(k)
 
-print('Histogram', histogram)
-print('Liczbna podejrzanych par:', len(suspectsPairs))
-print('Liczba podejrzanych osób:', len(suspectsPersons))
-print('Liczba podejrzanych par "osób i dni":', (int)(suspectsPersonsAndDays))
+    return {
+        "histogram": histogram,
+        "pp": len(suspects_pairs),
+        "po": len(suspects_persons),
+        "ppod": int(suspects_persons_and_days),
+    }
+
+
+n = int(input("Podaj liczbę osób (n):"))  # 10 000
+p = float(input("Podaj prawdopodobieństwo przenocowania w hotelu (p):"))  # 0.1
+h = int(input("Podaj liczbę hoteli (hn):"))  # 100
+d = int(input("Podaj liczbę dni (dn):"))  # 100
+x = int(input("Podaj liczbę przebiegów (x):"))
+
+results = []
+for _ in range(x):
+    results.append(simulate())
+
+sumResults = {}
+for item in results:
+    sumResults['pp'] = sumResults.get('pp', 0) + item['pp']
+    sumResults['po'] = sumResults.get('po', 0) + item['po']
+    sumResults['ppod'] = sumResults.get('ppod', 0) + item['ppod']
+
+print('Histogram', results[0]['histogram'])
+print('Liczbna podejrzanych par:', int(sumResults['pp'] / x))
+print('Liczba podejrzanych osób:', int(sumResults['po'] / x))
+print('Liczba podejrzanych par "osób i dni":', int(sumResults['ppod'] / x))
